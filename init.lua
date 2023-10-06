0141712e932fe58857ceefcb93d0c35954081b0e
Jeff Bigler <63808+jbigler@users.noreply.github.com>
2023-10-06 10:59:06 +0700

    Switch to Nordic theme.

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "nord",
+  colorscheme = "nordic",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
