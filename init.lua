5b0b51ea85f809679c85e90226eb90d00ad29531
Jeff Bigler <63808+jbigler@users.noreply.github.com>
2024-02-28 20:18:36 +0700

    Update custom settings

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "kanagawa-dragon",
+  colorscheme = "kanagawa-wave",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
--- plugins/community.lua
+++ plugins/community.lua
@@ -4,15 +4,17 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
   { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
   { import = "astrocommunity.editing-support.refactoring-nvim" },
   { import = "astrocommunity.editing-support.nvim-treesitter-endwise" },
   { import = "astrocommunity.file-explorer.oil-nvim" },
-  -- { import = "astrocommunity.utility.neodim" },
   { import = "astrocommunity.motion.flash-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.motion.vim-matchup" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.clojure" },
   { import = "astrocommunity.pack.lua" },
@@ -20,5 +22,4 @@ return {
   { import = "astrocommunity.pack.full-dadbod" },
   { import = "astrocommunity.utility.noice-nvim" },
   { import = "astrocommunity.workflow.hardtime-nvim" },
-  { import = "astrocommunity.completion.codeium-vim" },
 }
