984d16a2ad6ee0ce02617729d6278db29539f592
Jeff Bigler <63808+jbigler@users.noreply.github.com>
2024-03-21 00:10:51 +0700

    Change theme to darker dragon version of kanagawa

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "kanagawa-wave",
+  colorscheme = "kanagawa-dragon",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
